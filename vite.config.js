711cad8c654a47b0fb5d580f4fb3e3061bf65832
Matan Nahmani <matanmatan999new@gmail.com>
2024-10-01 02:49:02 +0900

    minor fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mirai-housing/', // Update this to match your GitHub repository name
-})
+  base: '/showcase/', // Update this to match your GitHub repository name
+});
